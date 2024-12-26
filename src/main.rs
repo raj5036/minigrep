9ef3316a5a67f93d0f264f0bb945f863b03126cd
Raj Karmakar <97rajmath@gmail.com>
2024-12-26 13:11:43 +0530

    feat: Read the arguments passed and save it in a Variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    dbg!(&args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("query is {}", query);
+    println!("filename is {}", filename);
 }
